0a05cdefc7b77eed6e1666e5143a053e333f573b
Travis Wright <twright@microsoft.com>
2016-10-26 23:39:28 -0400

    tweaked logic for better demo

--- node-connect-retry.js
+++ node-connect-retry.js
@@ -2,6 +2,7 @@ var mssql = require('tedious')
 
 //Use setInterval to retry the connection
 var connectionAttempts = 1;
+var connectionCount = 0;
 var connected = false;
 console.log("Attempting connection...");
 setInterval(function () {
@@ -9,34 +10,22 @@ setInterval(function () {
     var Connection = mssql.Connection;
     var config = {
         userName: 'sa'
-        ,password: 'sa'
-        ,server: 'localhost'
+        ,password: 'Yukon900'
+        ,server: '10.0.0.20'
         ,options: {database:'master'}
-        /* Uncomment if you need to debug connections/queries
-        ,debug:
-          {
-          packet: false,
-          data: false,
-          payload: false,
-          token: false,
-          log: false
-          }*/
         };
     var connection = new Connection(config);
     console.log("Connection attempt: " + connectionAttempts);
     connectionAttempts++;
-    /* Uncomment if you need to debug connections/queries
-    connection.on('infoMessage', infoError);
-    connection.on('errorMessage', infoError);
-    connection.on('debug', debug);
-    */
     connection.on('connect', function(err) {
       if (err) {
         console.log(err);
         connected = false;
+        delete connection;
       } else {
         connected = true;
-        console.log("Connected");
+        connectionCount++;
+        console.log("Connected. Connection count:" + connectionCount);
         setInterval(function() { getData(connection); }, 1000);
       }
     });
@@ -47,21 +36,22 @@ setInterval(function () {
 function getData(connection) {
   console.log("Getting Data...");
   var Request = require('tedious').Request;
-  request = new Request("SELECT HOST_NAME()", function(err, rowCount) {
+  request = new Request("SELECT CONVERT(sysname,SERVERPROPERTY('MachineName'))", function(err, rowCount) {
         if (err) {
-            connected = false;
             console.log(err);
+            connected = false;
+            delete connection;
         }
   });
   
   request.on('row', function(columns) { 
-            columns.forEach(function(column) {  
-              if (column.value === null) {  
-                console.log('NULL');
-              } else {  
-                console.log(column.value);
-              }  
-            });
+    columns.forEach(function(column) {  
+      if (column.value === null) {  
+        console.log('NULL');
+      } else {  
+        console.log(column.value);
+      }  
+    });
   });
 
   connection.execSql(request);  
